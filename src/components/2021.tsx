b604607faf889ff89cc398304ba30084eacb8e51
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2021-09-11 01:12:50 +0530

    added styles for 2021

--- src/components/2021.tsx
+++ src/components/2021.tsx
@@ -1,7 +1,22 @@
+import Body from "../assets/body.png";
+
 const TwentyOne = () => {
   return (
     <>
-      <div className="root21">2021</div>
+      <div className="root21">
+        <div className="root21__card">
+          <header className="root21__card__head">
+            <p className="root21__card__head__title">sugoi</p>
+            <p className="root21__card__head__menu">☰</p>
+          </header>
+          <div className="root21__card__body">
+            {/* <img className="root21__card__body__img" src={Body} alt="Body" /> */}
+          </div>
+          <div className="root21__card__foot">
+            <p>Copyright@{new Date().getFullYear()}</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
