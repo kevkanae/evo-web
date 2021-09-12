4b8388614de497573f5ccd2ca7b0eecda3f76102
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2021-09-12 16:42:01 +0530

    added more components

--- src/components/2003.tsx
+++ src/components/2003.tsx
@@ -1,7 +1,86 @@
+import { useState } from "react";
+
 const ZeroThree = () => {
+  const [selectedTabIndex, setTabIndex] = useState(0);
+
+  const handleTab = (i: number) => {
+    setTabIndex(i);
+  };
   return (
     <>
-      <div className="root03">2003</div>
+      <div className="root03">
+        <div className="root03__head">
+          <h2>Google in 2003</h2>
+        </div>
+        <div className="root03__body">
+          <div className="root03__body__title">
+            {[
+              { key: "G", color: "#1848B8" },
+              { key: "o", color: "#E24E38" },
+              { key: "o", color: "#C49E02" },
+              { key: "g", color: "#1848B8" },
+              { key: "l", color: "#54C855" },
+              { key: "e", color: "#E24E38" },
+            ].map((x, i) => (
+              <h2
+                style={{
+                  color: x.color,
+                }}
+                key={i}
+              >
+                {x.key}
+              </h2>
+            ))}
+          </div>
+          <div className="root03__body__nav">
+            {["Web", "Images", "Groups", "Directory", "News"].map((x, i) => (
+              <div
+                onClick={() => handleTab(i)}
+                className={
+                  selectedTabIndex === i
+                    ? "root03__body__nav__active"
+                    : "root03__body__nav__item"
+                }
+                key={i}
+              >
+                {x}
+              </div>
+            ))}
+          </div>
+          <div className="root03__body__content">
+            <div className="root03__body__content__left">
+              <input type="text" name="" id="" />
+              <div>
+                <button>Google Search</button>
+                <button>Im Feeling Lucky</button>
+              </div>
+            </div>
+            <div className="root03__body__content__right">
+              <ul>
+                <li>
+                  <a href="">Advanced Search</a>
+                </li>
+                <li>
+                  <a href="">Preferences</a>
+                </li>
+                <li>
+                  <a href="">Language Tools</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <h1>
+            <span style={{ color: "red" }}>New! </span>
+            Great Research. Small Fee. <a href="">Google Answers</a>
+          </h1>
+          <p>
+            <a href="">Advertise with us - </a>
+            <a href="">Search solutions - </a>
+            <a href="">Services and Tools - </a>
+            <a href="">Jobs, Press and Help</a>
+          </p>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/2010.tsx
+++ src/components/2010.tsx
@@ -1,7 +1,36 @@
+import A from "../assets/msa.png";
+import B from "../assets/msb.png";
+
 const Ten = () => {
   return (
     <>
-      <div className="root10">2010</div>
+      <div className="root10">
+        <div className="root10__head">
+          <h2>Microsoft in 2010</h2>
+        </div>
+        <div className="root10__body">
+          <div className="root10__body__nav">
+            {[
+              "Windows",
+              "Office",
+              "All Products",
+              "Buy Now",
+              "Trials",
+              "Solutions",
+              "Help",
+              "About Microsoft",
+            ].map((x, i) => (
+              <div className="root10__body__nav__item" key={i}>
+                {x}
+              </div>
+            ))}
+          </div>
+          <div className="root10__body__content">
+            <img className="root10__body__content__a" src={A} alt="a" />
+            <img className="root10__body__content__b" src={B} alt="a" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
